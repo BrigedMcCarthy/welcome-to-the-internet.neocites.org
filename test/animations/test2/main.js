923a512f1ccae9bd8e225410866c8406f70826cb
Briged McCarthy <168587753+BrigedMcCarthy@users.noreply.github.com>
2024-10-24 15:07:45 -0500

    removed test folder

--- test/animations/test2/main.js
+++
@@ -1,8 +0,0 @@
-let root = document.documentElement;
-let hotjarGraphic = document.getElementById("module-hotjar");
-
-hotjarGraphic.addEventListener("mousemove", e => {
-  console.log(e);
-  root.style.setProperty('--mouse-x', e.pageX - hotjarGraphic.offsetLeft + "px");
-  root.style.setProperty('--mouse-y', e.pageY - hotjarGraphic.offsetTop + "px");
-});
